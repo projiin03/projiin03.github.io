c93c980344f45f87012ea15c9ae8b9fcac6d5b07
projiin03 <projiin@naver.com>
2025-11-04 16:44:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// 스크롤 시 fade-in 등장
+// 스크롤 시 부드럽게 fade-in 등장
 const fadeEls = document.querySelectorAll('.fade-in');
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
@@ -6,7 +6,7 @@ const observer = new IntersectionObserver((entries) => {
       entry.target.classList.add('visible');
     }
   });
-});
+}, { threshold: 0.3 });
 
 fadeEls.forEach(el => observer.observe(el));
 
