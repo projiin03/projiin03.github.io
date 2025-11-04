c5be60ab8ea7644c63f444b2601f8a76663c05a0
projiin03 <projiin@naver.com>
2025-11-04 09:51:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 let slideIndex = 0;
 showSlides();
-
 function showSlides() {
   let slides = document.getElementsByClassName("slide");
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
+  for (let i = 0; i < slides.length; i++) slides[i].style.display = "none";
   slideIndex++;
-  if (slideIndex > slides.length) { slideIndex = 1 }
+  if (slideIndex > slides.length) slideIndex = 1;
   slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 4000); // 4초마다 전환
+  setTimeout(showSlides, 4000);
 }
